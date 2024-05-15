de2acc9d015d5349de9f07d92bf41f5e5f6774fe
DonavanC13 <donavanc13@outlook.com>
2024-05-15 11:40:07 -0400

    created and styled grid

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container');
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const grid = document.createElement('div');
+        grid.className = 'grid';
+        container.appendChild(grid);
+    }
+    const br = document.createElement('br');
+    container.appendChild(br);
+}
