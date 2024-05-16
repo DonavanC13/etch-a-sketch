5b7c414f51b79b49ae8f9997e4bc02a0e10b928c
DonavanC13 <donavanc13@outlook.com>
2024-05-16 13:26:34 -0400

    refined code and added new logic refined the way codes ran and added the logic to resize grid

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 const container = document.querySelector('#container');
+const resizeButton = document.getElementById('resizeButton');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+let gridSize = 16;
+
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
         const grid = document.createElement('div');
         grid.className = 'grid';
         container.appendChild(grid);
-    
+
+        grid.addEventListener('mouseover', function(){ 
+            grid.style.backgroundColor = '#4CAF50';
+        });
+    }    
     const br = document.createElement('br');
     container.appendChild(br);
-
-
-grid.addEventListener('mouseover', function(){ 
-    grid.style.backgroundColor = '#4CAF50';
-})
-    }
 }
+
+resizeButton.addEventListener('click', () => {
+    gridSize = prompt("Enter new grid size:");
+    container.innerHTML = '';
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const grid = document.createElement('div');
+            grid.className = 'grid';
+            container.appendChild(grid);
+
+            grid.addEventListener('mouseover', function() {
+                grid.style.backgroundColor = '#4CAF50';
+              });
+            }
+            const br = document.createElement('br');
+    container.appendChild(br);
+  }
+});
